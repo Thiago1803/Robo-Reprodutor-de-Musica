343479bee6b0a9bd8221b14004f1b413b0207136
Thiago Henrique dos Reis Magalhaes <thiago-maglhaes@hotmail.com>
2023-09-20 22:18:28 -0300

    mudandoCaminhoAudioTemp

--- view/act_talk_with_us.py
+++ view/act_talk_with_us.py
@@ -1,18 +1,29 @@
 import pygame
+import os
 from gtts import gTTS
 
 def falarMensagens(mensagem):
     # Cria um objeto gTTS com o texto desejado e com idioma pt-br
     tts = gTTS(text=mensagem, lang='pt-br')
 
-    # Salva o áudio em um arquivo temporário
-    tts.save("audio_temp.mp3")
+    pastaDestino = '/home/thiago/Área de Trabalho/TRABALHOIA/model/audioTemporario'
+    if not os.path.exists(pastaDestino):
+        os.makedirs(pastaDestino)
+
+    # Define o nome do arquivo de áudio temporário
+    nomeArquivo = "audio_temp.mp3"
+
+    # Caminho completo para o arquivo de áudio
+    caminhoCompleto = os.path.join(pastaDestino, nomeArquivo)
+
+    # Salva o áudio no caminho especificado
+    tts.save(caminhoCompleto)
 
     # Inicializa o mixer de áudio do pygame
     pygame.mixer.init()
 
     # Carrega o arquivo de áudio e reproduz
-    pygame.mixer.music.load("audio_temp.mp3")
+    pygame.mixer.music.load(caminhoCompleto)
     pygame.mixer.music.play()
 
     # Aguarda até que a reprodução termine
